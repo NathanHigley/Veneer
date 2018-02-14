4a089add59f4cf61309f9a37656717eb83fdf3bf
NathanHigley <nathan@jhigley.com>
2018-02-14 09:01:10 -0800

    pushpushpush

---
+++ input5-nmask-debug.py
@@ -0,0 +1,40 @@
+# input5-nmask-debug.py
+def getsubnet(cidr,nmask):
+	print('cidr',cidr,nmask)
+	cidr = int(cidr)
+	if cidr == 32:
+		nmask = [255,255,255,255]
+	if cidr == 31:
+		nmask = [255,255,255,254]
+	if cidr == 30:
+		nmask = [255,255,255,252]
+		if cidr == 32:
+		nmask = [255,255,255,255]
+	if cidr == 31:
+		nmask = [255,255,255,254]
+	if cidr == 30:
+		nmask = [255,255,255,252]Wind
+	print('print nmask',nmask)
+	return nmask
+
+
+def main():
+	cidr = -1
+	ipv4 = [-1,-1,-1,-1]
+	nmask =  [-1,-1,-1,-1]
+	netid = [-1,-1,-1,-1]
+	bcast = [-1,-1,-1,-1]
+	startingip = [-1,-1,-1,-1]
+	endingip = [-1,-1,-1,-1]
+	
+	print ('Input an IP and CIDR with 5 inputs.')
+	o1 = input("OCTET 1 : ")
+	o2 = input("OCTET 2 : ")
+	o3 = input("OCTET 3 : ")
+	o4 = input("OCTET 4 : ")
+	cidr = input("CIDR :")
+	print (o1,o2,o3,o4, "/", cidr)
+	ipv4[0] = o1; ipv4[1] = o2; ipv4[2] = o3; ipv4[3] = o4;   
+	nmask = getnmask(cidr,nmask)
+	print(ipv4,cidr,nmask,end='')
+main()
--- veneer_NH.py
+++ veneer_NH.py
@@ -1,6 +1,29 @@
-def main(args):
-    return 0
+def main():
+	nmask =  [-1,-1,-1,-1]
+	
+	cidr = input("CIDR : ")
+	
+	nmask[0] = 255; nmask[1] = 255;nmask[2] = 255;nmask[3] = 255;
+	x = 32-cidr
+	if (cidr < 32 and cidr > 23):
+		nmask[3] = 256-2**x;
+	if (cidr < 24 and cidr > 15):
+		x = 24-cidr
+		nmask[3] = 0;
+		nmask[2] = 256-2**x;
+	if (cidr < 16 and cidr > 7):
+		x = 16-cidr
+		nmask[3] = 0;
+		nmask[2] = 0;
+		nmask[1] = 256-2**x;
+	if (cidr < 8 and cidr > 0):
+		x = 8-cidr
+		nmask[3] = 0;
+		nmask[2] = 0;
+		nmask[1] = 0;
+		nmask[0] = 256-2**x;
+	if (cidr > 32 or cidr < 0 or cidr % 1 != 0):
+		print ("Invalid CIDR value.") 
+	print nmask
 
-if __name__ == '__main__':
-    import sys
-    sys.exit(main(sys.argv))
+main()
