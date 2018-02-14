822a6ece126d1a9ecfe61c413618927f341de4ce
NathanHigley <nathan@jhigley.com>
2018-02-14 09:29:21 -0800

    veiled

--- input5-nmask-debug.py
+++ input5-nmask-debug.py
@@ -8,12 +8,6 @@ def getsubnet(cidr,nmask):
 		nmask = [255,255,255,254]
 	if cidr == 30:
 		nmask = [255,255,255,252]
-		if cidr == 32:
-		nmask = [255,255,255,255]
-	if cidr == 31:
-		nmask = [255,255,255,254]
-	if cidr == 30:
-		nmask = [255,255,255,252]Wind
 	print('print nmask',nmask)
 	return nmask
 
--- veneer_NH.py
+++ veneer_NH.py
@@ -1,11 +1,16 @@
-def main():
+#NETMASK, CIDR
+def getnmask():
+	cidr = -1
 	nmask =  [-1,-1,-1,-1]
-	
 	cidr = input("CIDR : ")
 	
 	nmask[0] = 255; nmask[1] = 255;nmask[2] = 255;nmask[3] = 255;
-	x = 32-cidr
+	
+	while (cidr > 32 or cidr < 0 or cidr % 1 != 0):
+		print ("Invalid CIDR value.")
+		cidr = input("CIDR : ") 
 	if (cidr < 32 and cidr > 23):
+		x = 32-cidr
 		nmask[3] = 256-2**x;
 	if (cidr < 24 and cidr > 15):
 		x = 24-cidr
@@ -16,14 +21,17 @@ def main():
 		nmask[3] = 0;
 		nmask[2] = 0;
 		nmask[1] = 256-2**x;
-	if (cidr < 8 and cidr > 0):
+	if (cidr < 8 and cidr > 1):
 		x = 8-cidr
 		nmask[3] = 0;
 		nmask[2] = 0;
 		nmask[1] = 0;
 		nmask[0] = 256-2**x;
-	if (cidr > 32 or cidr < 0 or cidr % 1 != 0):
-		print ("Invalid CIDR value.") 
+	if (cidr == 0):
+		nmask[3] = 0;
+		nmask[2] = 0;
+		nmask[1] = 0;
+		nmask[0] = 0;
 	print nmask
 
-main()
+getnmask()
